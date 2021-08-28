0ebd3761f0eff9fbb5257a5f0b00990c0e07d809
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-08-28 21:08:47 +0900

    [Add] KakaoQR 프로젝트 생성(#1)

---
+++ KakaoQR/KakaoQR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KakaoQR
+//
+//  Created by SHIN YOON AH on 2021/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
