723517c5dbc3c1db277362a7f41024d5d85a7e5a
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-08-29 12:50:11 +0900

    [Add] View 생성(#3)

---
+++ KakaoQR/KakaoQR/Global/Constants/Const.swift
@@ -0,0 +1,15 @@
+//
+//  Const.swift
+//  KakaoQR
+//
+//  Created by SHIN YOON AH on 2021/08/29.
+//
+
+import Foundation
+
+struct Const {
+    struct Controller {
+        static let main = "MainVC"
+        static let qr = "QRCodeVC"
+    }
+}
---
+++ KakaoQR/KakaoQR/Source/Views/QRView.swift
@@ -0,0 +1,20 @@
+//
+//  QRView.swift
+//  KakaoQR
+//
+//  Created by SHIN YOON AH on 2021/08/29.
+//
+
+import UIKit
+
+class QRView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
